db311da0629d0d9596072ceffa9ed8a5b441656f
Andrey Lozovoy <lozovoya@gmail.com>
2020-09-10 12:52:48 +0300

    init commit

---
+++ cmd/bank/app/dto/card.go
@@ -0,0 +1,11 @@
+package dto
+
+type CardDTO struct {
+	Id       int    `json:"id"`
+	Number   string `json:"number"`
+	Issuer   string `json:"issuer"`
+	HolderId int    `json:"holder_id"`
+	Type     string `json:"type"`
+}
+
+type MessageDTO string
